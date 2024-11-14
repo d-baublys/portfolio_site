6ce7daadfd9cd1dd8b9e34a1895b204267a40856
dvbdbb <dovy1@hotmail.co.uk>
2024-11-14 15:29:21 +0000

    Add interactive section toggling

--- portfolio/static/portfolio/main.js
+++ portfolio/static/portfolio/main.js
@@ -1,1 +1,13 @@
 const sections = document.querySelectorAll(".split-section")
+
+function toggleSection(element) {
+    element.classList.toggle("active")
+}
+
+function setUpEventListeners() {
+    sections.forEach((section) => {
+        section.addEventListener("click", () => toggleSection(section))
+    })
+}
+
+setUpEventListeners();
