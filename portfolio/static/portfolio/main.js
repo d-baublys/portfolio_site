79725f89894bb08ef9548a32d9351384f49e7ad9
dvbdbb <dovy1@hotmail.co.uk>
2024-11-15 14:50:11 +0000

    Enable key press activation for split sections and active buttons

--- portfolio/static/portfolio/main.js
+++ portfolio/static/portfolio/main.js
@@ -6,20 +6,37 @@ function switchSection(section) {
     }
 }
 
+function keySectionSwitch(event, section) {
+    if (event.key === "Enter") {
+        switchSection(section);
+    }
+}
+
 function activateSection(section) {
     section.classList.add("active");
     sectionButtons = section.querySelectorAll(".button");
-    sectionButtons.forEach((button) => button.classList.add("active"));
+    sectionButtons.forEach((button) => enableButton(button));
 
     otherSection = sections[1 - Array.from(sections).indexOf(section)];
     otherSection.classList.remove("active");
     otherButtons = otherSection.querySelectorAll(".button");
-    otherButtons.forEach((button) => button.classList.remove("active"));
+    otherButtons.forEach((button) => disableButton(button));
+}
+
+function enableButton(button) {
+    button.classList.add("active");
+    button.tabIndex = 0;
+}
+
+function disableButton(button) {
+    button.classList.remove("active");
+    button.tabIndex = -1;
 }
 
 function setUpEventListeners() {
     sections.forEach((section) => {
         section.addEventListener("click", () => switchSection(section));
+        section.addEventListener("keydown", (event) => keySectionSwitch(event, section));
     });
 }
 
