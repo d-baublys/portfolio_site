83dcff23244c7bc27e170eb22b8439f9b9cfd885
dvbdbb <dovy1@hotmail.co.uk>
2025-07-29 10:49:08 +0100

    Adapt existing theme and scripts into scalable carousel layout

--- portfolio/static/portfolio/main.js
+++ portfolio/static/portfolio/main.js
@@ -1,39 +1,40 @@
-const sections = document.querySelectorAll(".split-section");
+const slides = document.querySelectorAll(".slider-item");
+const dotButtons = document.querySelectorAll(".carousel-dot");
 const switchAudio = document.getElementById("switch-audio");
 
-function switchSection(section) {
-    if (!section.classList.contains("active")) {
-        activateSection(section);
-        playAudio();
+let activeFeaturedIdx = 0;
+let isScrolling = false;
+
+function debounce(func, delay) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
+}
+
+function acrollActiveIntoView() {
+    const lastScroll = { top: window.scrollY, left: window.scrollX };
+
+    const element = document.getElementById(`site-${activeFeaturedIdx + 1}`);
+
+    if (element) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            inline: "center",
+            block: "nearest",
+        });
+
+        setTimeout(() => {
+            isScrolling = false;
+        }, 200);
     }
+    window.scrollTo(lastScroll);
 }
 
-function keySectionSwitch(event, section) {
-    if (event.key === "Enter") {
-        switchSection(section);
-    }
-}
-
-function activateSection(section) {
-    deactivateOtherActive();
-    section.classList.add("active");
-    toggleButtons(section.querySelectorAll(".button"), true);
-}
-
-function deactivateOtherActive() {
-    const activeSection = document.querySelector(".active");
-    if (activeSection) {
-        activeSection.classList.remove("active");
-        toggleButtons(activeSection.querySelectorAll(".button"), false);
-    }
-}
-
-function toggleButtons(buttons, isEnabled) {
-    buttons.forEach((button) => {
-        button.classList.toggle("active", isEnabled);
-        button.tabIndex = isEnabled ? 0 : -1;
-    });
-}
+const debouncedScrollIntoView = debounce(acrollActiveIntoView, 200);
 
 function playAudio() {
     switchAudio.volume = 0.2;
@@ -41,16 +42,61 @@ function playAudio() {
     switchAudio.play();
 }
 
+function switchSite() {
+    isScrolling = true;
+
+    const activeSite = document.querySelector(".slider-item.active");
+    const activeDot = document.querySelector(".carousel-dot.active");
+    if (activeSite && activeDot) {
+        activeSite.classList.remove("active");
+        activeSite.querySelectorAll(".site-button").forEach((button) => {
+            button.setAttribute("tabindex", "-1");
+        });
+        activeDot.classList.remove("active");
+    }
+
+    const newActiveSite = document.getElementById(`site-${activeFeaturedIdx + 1}`);
+    newActiveSite.classList.add("active");
+    newActiveSite.querySelectorAll(".site-button").forEach((button) => {
+        button.setAttribute("tabindex", "0");
+    });
+    document.getElementById(`carousel-dot-${activeFeaturedIdx + 1}`).classList.add("active");
+    debouncedScrollIntoView();
+    playAudio();
+}
+
+function navBack() {
+    if (isScrolling) return;
+    activeFeaturedIdx = activeFeaturedIdx > 0 ? activeFeaturedIdx - 1 : siteItemsLength - 1;
+    switchSite();
+}
+
+function navForward() {
+    if (isScrolling) return;
+    activeFeaturedIdx = activeFeaturedIdx < siteItemsLength - 1 ? activeFeaturedIdx + 1 : 0;
+    switchSite();
+}
+
+function handleDotButtonClick(button) {
+    const buttonIdx = parseInt(button.dataset.idx);
+    activeFeaturedIdx = buttonIdx;
+    switchSite();
+}
+
 function setUpEventListeners() {
-    sections.forEach((section) => {
-        section.addEventListener("click", () => switchSection(section));
-        section.addEventListener("keydown", (event) => keySectionSwitch(event, section));
+    document.getElementById("carousel-nav-back").addEventListener("click", () => navBack());
+    document.getElementById("carousel-nav-forward").addEventListener("click", () => navForward());
+
+    window.addEventListener("resize", debouncedScrollIntoView);
+
+    dotButtons.forEach((button) => {
+        button.addEventListener("click", () => handleDotButtonClick(button));
     });
 }
 
 function init() {
-    activateSection(sections[0]);
     setUpEventListeners();
+    switchSite();
 }
 
 init();
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.templatetags.static import static
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "portfolio/home.html")
+        context = {
+            "site_list": [
+                {
+                    "src": static("portfolio/images/ecommercepreview-1200w.png"),
+                    "srcset": f"{static("portfolio/images/ecommercepreview-480w.png")} 480w, {static("portfolio/images/ecommercepreview-768w.png")} 768w, {static("portfolio/images/ecommercepreview-1200w.png")} 1200w",
+                    "title": "E-commerce Site",
+                    "alt": "E-commerce Site Preview",
+                    "live_link": "https://db-wear.vercel.app/",
+                    "github_link": "https://github.com/d-baublys/ecommerce-site",
+                },
+                {
+                    "src": static("portfolio/images/taskapppreview-1200w.png"),
+                    "srcset": f"{static("portfolio/images/taskapppreview-480w.png")} 480w, {static("portfolio/images/taskapppreview-768w.png")} 768w, {static("portfolio/images/taskapppreview-1200w.png")} 1200w",
+                    "title": "Task App",
+                    "alt": "Task App Preview",
+                    "live_link": "https://task-management-app-bice-psi.vercel.app",
+                    "github_link": "https://github.com/d-baublys/task-management-app",
+                },
+                {
+                    "src": static("portfolio/images/blogpreview-1200w.png"),
+                    "srcset": f"{static("portfolio/images/blogpreview-480w.png")} 480w, {static("portfolio/images/blogpreview-768w.png")} 768w, {static("portfolio/images/blogpreview-1200w.png")} 1200w",
+                    "title": "Blog Site",
+                    "alt": "Blog Site Preview",
+                    "live_link": "https://db-blog.co.uk",
+                    "github_link": "https://github.com/d-baublys/django_blog",
+                },
+                {
+                    "src": static("portfolio/images/clonepreview-1200w.png"),
+                    "srcset": f"{static("portfolio/images/clonepreview-480w.png")} 480w, {static("portfolio/images/clonepreview-768w.png")} 768w, {static("portfolio/images/clonepreview-1200w.png")} 1200w",
+                    "title": "Blogger.com Clone",
+                    "alt": "Blogger Clone Preview",
+                    "live_link": "https://dbaublys.eu.pythonanywhere.com",
+                    "github_link": "https://github.com/d-baublys/blogger_clone",
+                },
+            ]
+        }
+
+        return render(request, "portfolio/home.html", context)
